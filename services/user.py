ab4fed90464e1afb8fbc323b3e2853e1ce50f8ed
Volodymyr Vitsenko <your@mail.com>
2025-04-12 16:42:24 +0200

    Cinema-management_system

--- services/user.py
+++ services/user.py
@@ -3,6 +3,7 @@ from django.db import transaction
 from django.contrib.auth import get_user_model
 
 
+@transaction.atomic
 def create_user(
         username: str,
         password: str,
@@ -10,18 +11,17 @@ def create_user(
         first_name: str = None,
         last_name: str = None,
 ) -> None:
-    with transaction.atomic():
-        user = get_user_model().objects.create_user(
-            username=username,
-            password=password,
-        )
-        if email:
-            user.email = email
-        if first_name:
-            user.first_name = first_name
-        if last_name:
-            user.last_name = last_name
-        user.save()
+    user = get_user_model().objects.create_user(
+        username=username,
+        password=password,
+    )
+    if email:
+        user.email = email
+    if first_name:
+        user.first_name = first_name
+    if last_name:
+        user.last_name = last_name
+    user.save()
 
 
 def get_user(user_id: int) -> get_user_model:
